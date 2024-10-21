700615235eb206e9a5cc3988a2b25baae5490241
Vishl1359 <vishaltilve111@gmail.com>
2024-10-21 20:55:49 +0530

    create_post added

---
+++ utils/api.ts
@@ -0,0 +1,83 @@
+export const login = async (
+  email: string,
+  password: string
+): Promise<string | null> => {
+  try {
+    const response = await fetch("https://app-74opign4ma-uc.a.run.app/api/admin/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      return data.token; // Assuming the API returns a token in the response
+    } else {
+      throw new Error("Login failed");
+    }
+  } catch (error) {
+    console.error("Login error:", error);
+    return null;
+  }
+};
+
+interface PostData {
+  title: string;
+  description: string;
+  post_type: string;
+  category: string;
+  source: string;
+  image?:string;
+}
+
+export const createPost = async (
+  formData: PostData,
+  token: string
+): Promise<void> => {
+  try {
+    const response = await fetch("https://app-74opign4ma-uc.a.run.app/api/post/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${token}`, // Include the token in the Authorization header
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = response.json();
+    if (!response.ok) {
+      throw new Error("Failed to create post");
+    }
+
+    // console.log("Post created successfully");
+    alert("Post createed succesfully");
+  } catch (error) {
+    console.error("Create post error:", error);
+  }
+};
+
+export const logout = async (token: string): Promise<string | null> => {
+  try {
+    const response = await fetch("http://localhost:3000/api/user/logout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${token}`,
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      return data.token; // Assuming the API returns a token in the response
+    } else {
+      throw new Error("Logout failed");
+    }
+  } catch (error) {
+    console.error("Logout error:", error);
+    return null;
+  }
+};
+
+
+
